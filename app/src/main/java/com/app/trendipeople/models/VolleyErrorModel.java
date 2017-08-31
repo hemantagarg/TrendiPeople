d49992b037f4bd12e1470c0e4e0a0b5ad4b2b085
hemanta <hemantagarg5@gmail.com>
2017-08-31 22:15:44 +0530

    ratingbar

--- app/src/main/java/com/app/trendipeople/models/VolleyErrorModel.java
+++ app/src/main/java/com/app/trendipeople/models/VolleyErrorModel.java
@@ -2,22 +2,6 @@ package com.app.trendipeople.models;
 
 import org.json.JSONObject;
 
-/****************************************************************
- * VolleyErrorModel.java
- * Created on July 3, 2017
- * Copyright (c) 2014, NETGEAR, Inc.
- * 350 East Plumeria, San Jose California, 95134, U.S.A.
- * All rights reserved.
- * This software is the confidential and proprietary information of
- * NETGEAR, Inc. ("Confidential Information"). You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with NETGEAR.
- *
- * @author VVDN
- *         Class Name: VolleyErrorModel
- *         Description: This class is used for getting/setting the type of volloey error and model its message
- ****************************************************************/
 public class VolleyErrorModel {
     boolean isResponseNeedToParse=false;
     String strMessage="";
